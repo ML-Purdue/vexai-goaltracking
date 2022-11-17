d488bd73388f39f0eb77d05936744994e391454b
Nicholas <beaker.wade@gmail.com>
2022-11-17 11:00:14 -0500

    overlaying predictions and running model on video works

--- GoalTracker.py
+++ GoalTracker.py
@@ -1,20 +1,38 @@
 import torch
 import cv2
+import sys
+import os
 from grip.BlueFilter import BlueGoalFilterCode
+from grip.RedFilter import RedGoalFilterCode
 
-cam = cv2.VideoCapture(0)
-detector = BlueGoalFilterCode()
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='11-10-22.pt', force_reload=True)
-model.cpu()
+def main():
+  model_input = sys.argv[1]
+  cam = cv2.VideoCapture(model_input)
+  detector = BlueGoalFilterCode()
+  model = torch.hub.load('ultralytics/yolov5', 'custom', path='11-10-22.pt', force_reload=True)
+  model.conf = 0.5
+  model.cpu()
 
-ret = True
-while ret:
-  ret, frame = cam.read()
-  detector.process(frame)
-  frame = detector.mask_output
-  cv2.imshow('frame', frame)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+  ret = True
+  while ret:
+    ret, frame = cam.read()
+    detector.process(frame)
+    frame = detector.mask_output
+    
+    pred = model(frame)
+    for row in pred.pandas().xyxy[0].iterrows():
+        pt1 = (int(row[1]["xmin"]), int(row[1]["ymin"]))
+        pt2 = (int(row[1]["xmax"]), int(row[1]["ymax"]))
+        color = (255, 0, 0)
+        thickness = 2
+        frame = cv2.rectangle(frame, pt1, pt2, color, thickness)
 
-cam.release()
-cv2.destroyAllWindows()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+
+  cam.release()
+  cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+  main()
